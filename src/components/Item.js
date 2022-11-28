1df16a59d1d24027b5a895c6e7861f468e064e20
Mohd Mubsshir Khan <kmubsshir@hotmail.com>
2022-11-28 15:25:05 +0530

    Uncomplited styles

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from './Item.module.css'
-import menu from '../data'
+import menu from './data';
 const Item = () => {
-    const { title, price, img, desc } = menu[0];
+    const { title, price, img, desc } = menu[6];
     return (
         <article className={styles.Item}>
             <img src={img} alt="" />
             <div className={styles.info}>
                 <div className={styles.heading}>
                     <h3>{title}</h3>
-                    <h3>{price}</h3>
+                    <h3 className={styles.price}>$ {price}</h3>
                 </div>
                 <p>{desc}</p>
             </div>
