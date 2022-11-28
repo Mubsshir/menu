8d1a48fcdc0d2da8a48e3f96d5b8baed2ae903f4
Mubsshir <39831189+Mubsshir@users.noreply.github.com>
2022-11-28 16:24:41 +0530

    Added mobile responsive css

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,7 +7,7 @@ const Item = (props) => {
             <div className={styles.info}>
                 <div className={styles.heading}>
                     <h3>{props.title}</h3>
-                    <h3 className={styles.price}>$ {props.price}</h3>
+                    <h3 className={styles.price}>${props.price}</h3>
                 </div>
                 <p>{props.desc}</p>
             </div>
