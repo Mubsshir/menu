8059da0ff1d29a55ce0407f17f9026c0607b5f3c
Mubsshir <39831189+Mubsshir@users.noreply.github.com>
2022-11-28 16:19:54 +0530

    Task completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,20 +1,17 @@
 import React from "react";
 import styles from './Item.module.css'
-import menu from './data';
-const Item = () => {
-    const { title, price, img, desc } = menu[6];
+const Item = (props) => {
     return (
         <article className={styles.Item}>
-            <img src={img} alt="" />
+            <img src={props.img} alt="" />
             <div className={styles.info}>
                 <div className={styles.heading}>
-                    <h3>{title}</h3>
-                    <h3 className={styles.price}>$ {price}</h3>
+                    <h3>{props.title}</h3>
+                    <h3 className={styles.price}>$ {props.price}</h3>
                 </div>
-                <p>{desc}</p>
+                <p>{props.desc}</p>
             </div>
         </article>
     );
 };
-
 export default Item;
--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -1,20 +1,27 @@
 import Item from "./Item";
 import styles from './MenuCard.module.css';
+import menu from './data';
+import { useState } from "react";
 const MenuCard = () => {
+    const [items, setItems] = useState(menu);
+    const changeCategory = (cat) => {
+        if (cat === "All") {
+            setItems(menu);
+            return;
+        }
+        let newItems = menu.filter(item => item.category === cat);
+        setItems(newItems);
+    }
     return (
         <main className={styles.MenuCard}>
             <h3 className={styles.heading}>Our Menus</h3>
             <nav>
                 <ul>
-                    <li>All</li>
-                    <li>Breakfast</li>
-                    <li>Lunch</li>
-                    <li>Shakes</li>
+                    {["All", ...new Set(menu.map((item) => item.category))].map((cat, index) => <li onClick={() => { changeCategory(cat) }} key={index}>{cat}</li>)}
                 </ul>
             </nav>
             <section className={styles.itemContainer}>
-                <Item />
-                <Item />
+                {items.map(item => <Item key={item.id} img={item.img} title={item.title} price={item.price} desc={item.desc} />)}
             </section>
         </main>
     )
